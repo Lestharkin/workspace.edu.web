33346bb6b202f69645db20ce726f0f38c6ba08e5
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2023-08-16 09:48:43 -0500

    sync

--- mvc/js/local/index/controller/IndexController.js
+++ mvc/js/local/index/controller/IndexController.js
@@ -1,3 +1,13 @@
 export default class IndexController {
   
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+    this.view.display(this.getMovies());
+  }
+
+  getMovies = () => {
+    // aquí pasan muchas cosas de control
+    return this.model.getMovies();
+  }
 }
--- mvc/js/local/index/model/IndexModel.js
+++ mvc/js/local/index/model/IndexModel.js
@@ -1,3 +1,20 @@
 export default class IndexModel {
+
+  constructor() {
+    this.name = 'IndexModel';
+  }
+
+  async getMovies() {
+    const response = await fetch('https://swapi.dev/api/films');
+    const movies = await response.json();
+    return movies.results.map( movie => {
+      return {
+        title: movie.title,
+        director: movie.director,
+        producer: movie.producer,
+        release_date: movie.release_date
+      }
+    });
+  }
   
 }
