2e7268abea7a6141150ca7c16bd548d7784bc2f5
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2023-08-29 10:59:22 -0500

    sync

---
+++ typescript-mvc/src/index/model/IndexModel.ts
@@ -0,0 +1,9 @@
+export default class IndexModel {
+  constructor () {
+    console.log('IndexModel constructor')
+  }
+
+  getData = (): Promise<> => {
+
+  }
+}
---
+++ typescript-mvc/src/index/view/IndexView.ts
@@ -0,0 +1,5 @@
+export default class IndexView {
+  constructor () {
+    console.log('IndexView constructor')
+  }
+}
