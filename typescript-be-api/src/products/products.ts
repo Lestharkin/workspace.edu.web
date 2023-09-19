6cab8b5345c52b68104ff6b82ba68fc39b1f3173
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2023-09-19 09:42:27 -0500

    sync

--- typescript-be-api/src/products/products.ts
+++ typescript-be-api/src/products/products.ts
@@ -1,7 +1,29 @@
 import express, { Application } from 'express'
+import path from 'path'
+import dotenv from 'dotenv'
 class Server {
   products: Application
 
+  constructor () {
+    this.products = express()
+    this.config()
+  }
+
+  config = (): void => {
+    dotenv.config({
+      path: path.join(__dirname, '../../config/.env.development')
+    })
+    this.products.use(express.json())
+    this.products.use(express.urlencoded({ extended: true }))
+  }
+
+  start = (): void => {
+    const PORT = process.env.PORT ?? 3000
+    const HOST = process.env.HOST ?? 'localhost'
+    this.products.listen(PORT, () => {
+      console.info(`SERVER: http://${HOST}:${PORT}`)
+    })
+  }
 }
 
 const server = new Server()
