52c857755fdf92ab3db9c43afdc96ef8d9150470
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2023-09-26 08:53:33 -0500

    sync

---
+++ typescript-be-api/src/products/types/ProductsInterface.ts
@@ -0,0 +1,11 @@
+export interface ProductsInterface {
+  id: number
+  title: string
+  amount: string
+  price: number
+  description: string
+  favorite: boolean
+  discount: boolean
+  discountPer: number
+  discountUni: string
+}
