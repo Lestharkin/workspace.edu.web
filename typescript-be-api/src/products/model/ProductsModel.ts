8586e7f78e9168396d1dec59ce0dcfb11d868380
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2023-09-26 09:02:04 -0500

    sync

--- typescript-be-api/src/products/model/ProductsModel.ts
+++ typescript-be-api/src/products/model/ProductsModel.ts
@@ -1,5 +1,14 @@
+import { ProductInterface } from '../types/ProductInterface'
+import products from '../database/products.json'
+
 export default class ProductsModel {
-  getProducts = (): void => {
-    console.log('ProductsModel.getProducts()')
+  getProducts = async (): Promise<ProductInterface[]> => {
+    return await new Promise((resolve, reject) => {
+      try {
+        resolve(products)
+      } catch (error) {
+        reject(error)
+      }
+    })
   }
 }
