9fc6baa2d2b557015d2c35334b47b34716c9a83c
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2023-10-17 10:41:26 -0500

    sync

---
+++ products-api-hex/src/product/infrastructure/repository/mongo/ProductRepositoryMongoAdapter.ts
@@ -0,0 +1,16 @@
+import ProductModel from '../../../domain/model/ProductModel'
+import ProductRepositoryPort from '../../../domain/port/driven/ProductRepositoryPort'
+
+export default class ProductMongodbAdapter implements ProductRepositoryPort<ProductModel> {
+  findById = async (id: string): Promise<ProductModel | undefined> => {
+    return undefined
+  }
+
+  findAll = async (): Promise<ProductModel[] | []> => {
+    return []
+  }
+
+  findProductsByPrice = async (min: string, max: string): Promise<ProductModel[] | []> => {
+    return []
+  }
+}
