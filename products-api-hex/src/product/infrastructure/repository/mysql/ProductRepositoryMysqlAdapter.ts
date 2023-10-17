3e1bbc970a136061fdfb53f78c2d38273d0b8983
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2023-10-17 08:43:07 -0500

    sync

---
+++ products-api-hex/src/product/infrastructure/repository/mysql/ProductRepositoryMysqlAdapter.ts
@@ -0,0 +1,19 @@
+import ProductModel from '../../../domain/model/ProductModel'
+import ProductRepositoryPort from '../../../domain/port/driven/ProductRepositoryPort'
+
+export default class ProductRepositoryMysqlAdapter implements ProductRepositoryPort<ProductModel> {
+  findById = async (id: string): Promise<ProductModel | undefined> => {
+    // TODO: implement
+    return undefined
+  }
+
+  findAll = async (): Promise<ProductModel[] | []> => {
+    // TODO: implement
+    return []
+  }
+
+  findProductsByPrice = async (min: string, max: string): Promise<ProductModel[] | []> => {
+    // TODO: implement
+    return []
+  }
+}
