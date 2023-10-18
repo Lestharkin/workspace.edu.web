9f2c1101928ba7eb6a8dbb2ec29a67d6ba08f5ac
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2023-10-18 09:11:43 -0500

    sync

--- products-api-hex/src/product/infrastructure/repository/json/ProductRepositoryJsonAdapter.ts
+++ products-api-hex/src/product/infrastructure/repository/json/ProductRepositoryJsonAdapter.ts
@@ -1,9 +1,34 @@
 import ProductModel from '../../../domain/model/ProductModel'
 import ProductRepositoryPort from '../../../domain/port/driven/ProductRepositoryPort'
+import products from './database/products.json'
 
 export default class ProductRepositoryJsonAdapter implements ProductRepositoryPort<ProductModel> {
   findById = async (id: string): Promise<ProductModel | undefined> => {
-    return undefined
+    return await new Promise((resolve, reject) => {
+      try {
+        const product = products.find((product) => product.id === Number(id))
+        if (product !== undefined) {
+          resolve(new ProductModel({
+            id: product.id,
+            title: product.title,
+            amount: product.amount,
+            price: product.price,
+            description: product.description,
+            favorite: product.favorite,
+            discount: {
+              discountCh: product.discount,
+              discountPer: product.discountPer,
+              discountUni: product.discountUni
+            },
+            image: `${product.id}.jpg`
+          }))
+        } else {
+          reject(new Error('Product not found'))
+        }
+      } catch (error) {
+        reject(error)
+      }
+    })
   }
 
   findAll = async (): Promise<ProductModel[] | []> => {
