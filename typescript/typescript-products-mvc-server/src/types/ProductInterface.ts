0893835c6da334f7ea0d867de35a03c9700b9a37
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2024-04-02 10:42:10 -0500

    sync

---
+++ typescript/typescript-products-mvc-server/src/types/ProductInterface.ts
@@ -0,0 +1,11 @@
+export default interface ProductInterface {
+  id:          number;
+  title:       string;
+  amount:      string;
+  price:       number;
+  description: string;
+  favorite:    boolean;
+  discount:    boolean;
+  discountPer: number;
+  discountUni: string;
+}
