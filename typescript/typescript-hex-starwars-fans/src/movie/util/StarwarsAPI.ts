388e6ba8013fbe757195566b8c354e462546e34c
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2024-04-22 10:51:33 -0500

    sync

--- typescript/typescript-hex-starwars-fans/src/movie/util/StarwarsAPI.ts
+++ typescript/typescript-hex-starwars-fans/src/movie/util/StarwarsAPI.ts
@@ -5,22 +5,20 @@ export default class StarwarsAPI {
   private uriPeople = 'https://swapi.dev/api/people/'
 
 
-  public fetchAllMovies(): Movie[] {
+  public async fetchAllMovies(): Promise<Movie[]> {
     let movies: Movie[] = []
-    fetch(this.uriFilms)
-    .then((response) => response.json())
-    .then((data) => {
-      movies = data.results.map((movie: any) => {
-        return {
-          title: movie.title,
-          episode_id: movie.episode_id,
-          opening_crawl: movie.opening_crawl,
-          release_date: movie.release_date,
-          director: movie.director,
-          producer: movie.producer,
-          characters: movie.characters
-        }
-      })
+    const response = await fetch(this.uriFilms)
+    const data = await response.json()
+    movies = data.results.map((movie: any) => {
+      return {
+        title: movie.title,
+        episode_id: movie.episode_id,
+        opening_crawl: movie.opening_crawl,
+        release_date: movie.release_date,
+        director: movie.director,
+        producer: movie.producer,
+        characters: movie.characters
+      }
     })
     return movies
   }
