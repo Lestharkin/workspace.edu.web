2ec86e5420c249ba153b9b1737f2566c4aecb2d6
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2024-04-23 11:18:14 -0500

    sync

---
+++ typescript/typescript-hex-starwars-fans-martes/src/movie/domain/starwars/StarwarsMovie.ts
@@ -0,0 +1,9 @@
+export default interface StarwarsMovie {
+  title: string
+  episode_id: number
+  opening_crawl: string
+  release_date: string
+  director: string
+  producer: string
+  characters: string[]
+}
