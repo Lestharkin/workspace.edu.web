bb843e822527360b7209abb829ee4e9836d58e0e
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2024-04-25 11:16:26 -0500

    sync

--- typescript/typescript-hex-starwars-fans-lunes/src/movie/infrastructure/repository/RetrieveMovies.ts
+++ typescript/typescript-hex-starwars-fans-lunes/src/movie/infrastructure/repository/RetrieveMovies.ts
@@ -1,15 +1,18 @@
 import Character from "../../domain/model/character/Character"
 import NullCharacter from "../../domain/model/character/NullCharacter"
+import Director from "../../domain/model/director/Director"
+import NullDirector from "../../domain/model/director/NullDirector"
 import Movie from "../../domain/model/movie/Movie"
 import NullMovie from "../../domain/model/movie/NullMovie"
 import AbstractPerson from "../../domain/model/person/AbstractPerson"
+import NullProducer from "../../domain/model/producer/NullProducer"
+import Producer from "../../domain/model/producer/Producer"
 import RetrieveMoviesPort from "../../domain/port/driven/RetrieveMoviesPort"
 import StarwarsMovie from "../../domain/starwars/StarwarsMovie"
 import StarwarsAPI from "../../util/StarwarsAPI"
 
 export default class RetrieveMovies implements RetrieveMoviesPort {
   constructor(private readonly starwarsAPI: StarwarsAPI) {}
-
 
   public findAll = async (): Promise<Movie[]> => {
     const starwarsMovies = await this.starwarsAPI.fetchAllMovies()
@@ -61,10 +64,36 @@ export default class RetrieveMovies implements RetrieveMoviesPort {
   }
 
   private getProducers = async (starwarsMovie: StarwarsMovie): Promise<AbstractPerson[]> => {
-
+    const starwarsProducers = starwarsMovie.producer.split(", ")
+    return starwarsProducers.map((starwarsProducer): AbstractPerson => {
+      const {name, lastname} = this.splitNames(starwarsProducer)
+      if(this.isEmpty(name) || this.isEmpty(lastname)) {
+        return new NullProducer()
+      }
+      return new Producer(
+        name,
+        lastname
+      )
+    })
   }
 
   private getDirector = async (starwarsMovie: StarwarsMovie): Promise<AbstractPerson> => {
+    const starwarsDirector = starwarsMovie.director
+    const {name, lastname} = this.splitNames(starwarsDirector)
+    if(this.isEmpty(name) || this.isEmpty(lastname)) {
+      return new NullDirector()
+    }
+    return new Director(
+      name,      
+      lastname
+    )
+  }
 
+  private splitNames = (starwarsNames: string): {name: string, lastname: string} => {
+    const names = starwarsNames.split(" ")
+    return {
+      name: names[0],
+      lastname: names[1]
+    }
   }
 }
