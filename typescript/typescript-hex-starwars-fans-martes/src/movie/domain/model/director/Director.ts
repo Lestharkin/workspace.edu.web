be924e387eaae400703ca972d14d616767b937c6
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2024-04-29 09:37:44 -0500

    sync

--- typescript/typescript-hex-starwars-fans-martes/src/movie/domain/model/director/Director.ts
+++
@@ -1,7 +0,0 @@
-import AbstractPerson from '../person/AbstractPerson'
-
-export default class Director extends AbstractPerson {
-  public isNull(): boolean {
-    return false
-  }
-}
--- typescript/typescript-hex-starwars-fans-martes/src/movie/domain/model/producer/Producer.ts
+++
@@ -1,7 +0,0 @@
-import AbstractPerson from '../person/AbstractPerson'
-
-export default class Producer extends AbstractPerson {
-  public isNull(): boolean {
-    return false
-  }
-}
