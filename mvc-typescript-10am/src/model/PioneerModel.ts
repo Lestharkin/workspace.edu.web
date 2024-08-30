0bc2a9d2510ba82f0d4d333f59d4b221276eb358
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2024-08-30 08:28:16 -0500

    sync

--- mvc-typescript-10am/src/model/PioneerModel.ts
+++ mvc-typescript-10am/src/model/PioneerModel.ts
@@ -1,4 +1,22 @@
+import Pioneer from '../types/Pioneer.js'
 import Subject from '../types/Subject.js'
 import PioneerView from '../view/PioneerView.js'
 
-export default class PioneerModel extends Subject<PioneerView> {}
+export default class PioneerModel extends Subject<PioneerView> {
+  private pioneers: Pioneer[]
+
+  constructor(pioneers: Pioneer[]) {
+    super()
+    this.pioneers = pioneers
+  }
+
+  public getPioneers(): Pioneer[] {
+    return this.pioneers
+  }
+
+  public deletePioneer(id: number): void {
+    const index = this.pioneers.findIndex((pioneer) => pioneer.id === id)
+    this.pioneers.splice(index, 1)
+    this.notifyAllObservers()
+  }
+}
