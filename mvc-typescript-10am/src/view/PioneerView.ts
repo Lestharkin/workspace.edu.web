9bb938bf9b013cdd75f9839f13292671c446b6bc
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2024-08-30 08:45:01 -0500

    sync

--- mvc-typescript-10am/src/view/PioneerView.ts
+++ mvc-typescript-10am/src/view/PioneerView.ts
@@ -2,6 +2,13 @@ import PioneerModel from '../model/PioneerModel.js'
 import Observer from '../types/Observer.js'
 
 export default class PioneerView extends Observer<PioneerModel> {
+  private selector: HTMLDivElement
+
+  constructor(subject: PioneerModel) {
+    super(subject)
+    this.selector = document.querySelector('pioneers') as HTMLDivElement ?? document.createElement('pioneers')
+  }
+  
   public update(): void {
     console.log('PioneerView update')
   }
