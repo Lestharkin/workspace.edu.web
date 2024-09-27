7acd81cbb2e1ad6f243b11f33891a84d4b43e47d
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2024-09-27 10:02:31 -0500

    sync

--- backend/mvc-movies-server-10am/src/client/public/js/index/model/IndexModel.js
+++
@@ -1,5 +0,0 @@
-export default class IndexModel {
-    init() {
-        console.log('IndexModel initialized');
-    }
-}
--- backend/mvc-movies-server-2pm/init.sh
+++
@@ -1,4 +0,0 @@
-npm init -y
-npm install express
-npm install typescript ts-node-dev ts-node @types/node @types/express -D
-tsc --init
