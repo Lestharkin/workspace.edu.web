a94911a4756d237e8b18f30ed9b36dfe0e354a5a
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2024-09-30 10:36:46 -0500

    SYNC

---
+++ frontend/mvc-movies-client/src/contact/template/ContactTemplate.ts
@@ -0,0 +1,29 @@
+export default class ContactTemplate {
+  public static render(): string {
+    return `
+      <div class="contact-form">
+        <h2>Contáctenos</h2>
+        <form>
+          <div class="form-control">
+            <label for="name">Nombre:</label>
+            <input type="text" id="name" name="name" required>
+          </div>
+
+          <div class="form-control">
+            <label for="email">Correo Electrónico:</label>
+            <input type="email" id="email" name="email" required>
+          </div>
+
+          <div class="form-control">
+            <label for="message">Mensaje:</label>
+            <textarea id="message" name="message" required></textarea>
+          </div>
+
+          <div class="form-buttons">
+            <button type="submit">Enviar</button>
+          </div>
+        </form>
+      </div>
+    `
+  }
+}
