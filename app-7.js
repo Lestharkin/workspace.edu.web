6891692cebc91cab43a2d5d8fd8cc0e72dc4ec47
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-01-23 17:23:55 -0500

    sync

---
+++ app-7.js
@@ -0,0 +1,20 @@
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if(false) {
+      reject('Promise 1 rejected');    
+    }    
+    resolve('Promise 1 resolved');  
+  }, 300)
+});
+
+console.log(promise1);
+
+promise1.then(value => {
+  console.log(value);
+}).catch(error => {
+  console.log(error);
+});
+
+const w = async () => await promise1
+console.log(w());
+console.log(await w());
