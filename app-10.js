6cd584ccdcbef462ff97232f0578de1b92e56429
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-01-28 11:02:48 -0500

    sync

--- app-10.js
+++ app-10.js
@@ -1,37 +1,48 @@
-const board = [];
-const SIZE = 4
+const board = []
+const SIZE = {
+  rows: 4,
+  columns: 4,
+  length: () => SIZE.rows * SIZE.columns
+} 
+const nMines = Math.floor(Math.random() * (SIZE.length() / 2)) + 1
 
-function fill(matrix, rows, columns) {
-  for(let i = 0; i < rows; i++){
-    const array =[]
-     for(let j = 0; j < columns; j++){
-       array.push('x');
-     }
-     matrix.push(array);
-   };
+function main () {
+  console.log('## Minesweeper ##')
+
+  fill(board, SIZE.rows, SIZE.columns)
+  fillMines(board, nMines)
+
+  print(board)
 }
 
-function print(matrix) {
-  for(let i = 0; i < matrix.length; i++){
-    let line = ''
-     for(let j = 0; j < matrix[i].length; j++){
-       line = line + matrix[i][j] + ' ';
-     }
-      console.log(line);
-   };
+const fill = (matrix, rows, columns) => {
+  for (let i = 0; i < rows; i++) {
+    matrix[i] = []
+    for (let j = 0; j < columns; j++) {
+      matrix[i][j] = 'x'
+    }
+  };
 }
 
-const nMines = Math.floor(Math.random() * (SIZE*SIZE/2)) + 1;
 const fillMines = (matrix, n) => {
-  for(let i = 0; i < n; i++){
-    const x = Math.floor(Math.random() * SIZE);
-    const y = Math.floor(Math.random() * SIZE);
-    matrix[x][y] = 'M';
+  for (let i = 0; i < n; i++) {
+    const x = Math.floor(Math.random() * SIZE.rows)
+    const y = Math.floor(Math.random() * SIZE.columns)
+    matrix[x][y] = 'M'
   }
 }
-fill(board, SIZE, SIZE);
-fillNines(board, nNimes);
 
-print(board);
+function print (matrix) {
+  for (const row of matrix) {
+    let line = ''
+    for (const cell of row) {
+      line = line + cell + ' '
+    }
+    console.log(line)
+  };
+}
+
+main();
+
 
 
