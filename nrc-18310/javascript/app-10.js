da4c3fba1c4ae097d28115fb422717542987248d
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-01-31 11:14:19 -0500

    sync

--- nrc-18310/javascript/app-10.js
+++ nrc-18310/javascript/app-10.js
@@ -6,12 +6,27 @@ const dim = {
   length: () => dim.rows * dim.columns
 }
 
-for (let i = 0; i < dim.rows; i++) {
-  let row = [];
-  for (let j = 0; j < dim.columns; j++) {
-    row.push('x');
-  }  
-  board.push(row);
+function initBoard() {
+  for (let i = 0; i < dim.rows; i++) {
+    let row = [];
+    for (let j = 0; j < dim.columns; j++) {
+      row.push('x');
+    }  
+    board.push(row);
+  }
 }
 
-console.table(board);
+function showBoard() {
+  for (let i = 0; i < dim.rows; i++) {
+    let line = '';
+    for (let j = 0; j < dim.columns; j++) {
+      line += board[i][j] + ' ';
+    }  
+    console.log(line);
+  }
+}
+
+
+// Main
+initBoard();
+showBoard();
