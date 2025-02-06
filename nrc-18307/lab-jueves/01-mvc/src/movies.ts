ab65887a9c15b036cc39b09963e65455bcd6cc22
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-02-06 17:34:33 -0500

    sync

--- nrc-18307/lab-jueves/01-mvc/src/movies.ts
+++ nrc-18307/lab-jueves/01-mvc/src/movies.ts
@@ -1,1 +1,11 @@
-console.log("Hello juju!");
+import Server from './express/Server'
+import MovieController from './movie/controller/MovieController'
+import MovieModel from './movie/model/MovieModel'
+import MovieView from './movie/view/MovieView'
+
+const movieModel = new MovieModel()
+const movieController = new MovieController(movieModel)
+const movieView = new MovieView(movieController)
+
+const server = new Server(movieView)
+server.start()
