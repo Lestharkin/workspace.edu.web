bbdaba7369d4809a305654ad665395d6fb8c5a29
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-02-07 10:51:20 -0500

    sync

---
+++ nrc-18310/01-mvc/init.sh
@@ -0,0 +1,8 @@
+# Description: Initialize a new Node.js project with TypeScript
+npm init -y
+# dependencies
+npm install express
+# devDependencies
+npm install typescript ts-node ts-node-dev @types/node @types/express -D
+# tsconfig.json
+tsc --init
