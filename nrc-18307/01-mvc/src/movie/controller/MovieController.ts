ee3f193bfbdfe42824fbfebfda998e7217bc8d3e
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-02-10 11:18:42 -0500

    sync

---
+++ nrc-18307/01-mvc/src/movie/controller/MovieController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express'
+import MovieModel from '../model/MovieModel'
+
+export default class MovieController {
+  constructor(private readonly movieModel: MovieModel) {}
+
+  public getMovies(_req: Request, res: Response): void {
+    res.status(200).json({ movies: this.movieModel.retrieveMovies() })
+  }
+}
