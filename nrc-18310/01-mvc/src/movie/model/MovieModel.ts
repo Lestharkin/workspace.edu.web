70dae7ab8942102651cb2ca31c81efb08b3bde64
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-02-13 14:15:13 -0500

    sync

--- nrc-18310/01-mvc/src/movie/model/MovieModel.ts
+++ nrc-18310/01-mvc/src/movie/model/MovieModel.ts
@@ -3,6 +3,19 @@ import movies_json from '../../../database/movies-2020s.json'
 
 export default class MovieModel {
   public retrieveMovies(): Movie[] {
-    return movies_json as Movie[]
+    const movies =  (movies_json as Movie[]).map((movie) => {
+      return {
+        price: movie.price,
+        title: movie.title,
+        year: movie.year,
+        score: movie.score,
+        cast: movie.cast,
+        genres: movie.genres,
+        extract: movie.extract,
+        thumbnail: `${Environment.getProtocol()}://${Environment.getHost()}:${Environment.getPort()}/api/v1.0/movies/movie/image/${
+          movie.thumbnail
+        }`,
+      }
+    })
   }
 }
