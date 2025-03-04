2596b401c151b3410aca1bf7b9f4401a41a52843
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-03-04 16:42:58 -0500

    sync

--- nrc-18307/lab-martes/movies-hex-backend/src/movies/infrastructure/repository/swapi/FilmsToMovies.ts
+++ nrc-18307/lab-martes/movies-hex-backend/src/movies/infrastructure/repository/swapi/FilmsToMovies.ts
@@ -1,6 +1,37 @@
 import SWMovieInterface from '../../../../swapi/domain/SWMovieInterface'
+import Director from '../../../domain/director/Director'
+import NullDirector from '../../../domain/director/NullDirector'
 import Movie from '../../../domain/movie/Movie'
 
 export default class FilmsToMovies {
-  public static get = (swMovieInterface: SWMovieInterface[]): Movie[] => {}
+  public get = (swMovieInterface: SWMovieInterface[]): Movie[] => {
+    const movies = swMovieInterface.map((swMovie) => {
+      return new Movie({
+        title: swMovie.title,
+        price: 0,
+        year: parseInt(swMovie.release_date),
+        genres: '',
+        extract: swMovie.opening_crawl,
+        director: this.getDirector(swMovie.director),
+        producer: swMovie.producer,
+        age: 0,
+        image: [],
+        characters: []
+      })
+    })
+  }
+
+  private readonly getDirector = (director: string): Director => {
+    const directorNames = director.split(' ')
+
+    if(directorNames[0] === undefined || directorNames[0] === null || directorNames[0] === '') {
+      return new NullDirector()
+    }
+
+    return new Director({
+      names: directorNames[0] ?? "",
+      surnames: directorNames[1] ?? "",
+      yearsOfExperience: 0
+    })
+  }
 }
