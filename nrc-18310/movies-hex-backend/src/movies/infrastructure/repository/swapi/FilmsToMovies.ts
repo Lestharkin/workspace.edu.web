5bb64fa8df8d537853a834adbc42b8c0b0f6fb3f
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-03-07 10:46:49 -0500

    sync

--- nrc-18310/movies-hex-backend/src/movies/infrastructure/repository/swapi/FilmsToMovies.ts
+++ nrc-18310/movies-hex-backend/src/movies/infrastructure/repository/swapi/FilmsToMovies.ts
@@ -11,8 +11,8 @@ export default class FilmsToMovies {
     private readonly getterCharacters: GetterCharacters
   ) {}
 
-  public get = (films: SWMovieInterface[]): Movie[] => {
-    return films.map((film) => {
+  public get = (films: SWMovieInterface[]): Promise<Movie[]> => {
+    const movies = films.map(async (film) => {
       return new Movie({
         title: film.title,
         price: 0,
@@ -23,8 +23,10 @@ export default class FilmsToMovies {
         producer: this.getterProducers.get(film.producer),
         age: 0,
         image: [],
-        characters: this.getterCharacters.get(film),
+        characters: await this.getterCharacters.get(film),
       })
     })
+
+    return Promise.all(movies)
   }
 }
