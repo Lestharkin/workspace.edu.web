b7aab6dd4bb1510362e7a7baac0bf735686b2bda
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-03-07 11:04:47 -0500

    sync

--- nrc-18310/movies-hex-backend/src/movies/infrastructure/factory/MovieRouterFactory.ts
+++ nrc-18310/movies-hex-backend/src/movies/infrastructure/factory/MovieRouterFactory.ts
@@ -1,12 +1,37 @@
 import RouterExpressInterface from '../../../express/domain/RouterExpressInterface'
+import SWAPI from '../../../swapi/infrastructure/SWAPI'
+import MovieService from '../../application/service/MovieService'
 import MovieUseCase from '../../application/usecase/MovieUseCase'
 import MovieControllerExpress from '../express/controller/MovieControllerExpress'
 import MovieRouterExpress from '../express/router/MovieRouterExpress'
+import FilmsToMovies from '../repository/swapi/FilmsToMovies'
+import GetterCharacters from '../repository/swapi/GetterCharacters'
+import GetterDirector from '../repository/swapi/GetterDirector'
+import GetterProducers from '../repository/swapi/GetterProducers'
+import SWMovieRepository from '../repository/swapi/SWMovieRepository'
 
 export default class MovieRouterFactory {
   public static create(): RouterExpressInterface {
-    const movieUseCase = new MovieUseCase()
+    const swapi = new SWAPI()
+
+    const getterDirector = new GetterDirector()
+    const getterProducers = new GetterProducers()
+    const getterCharacters = new GetterCharacters(swapi)
+
+    const filmsToMovies = new FilmsToMovies(
+      getterDirector,
+      getterProducers,
+      getterCharacters
+    )
+
+    const swMovieRepository = new SWMovieRepository(swapi, filmsToMovies)
+
+    const movieService = new MovieService(swMovieRepository)
+
+    const movieUseCase = new MovieUseCase(movieService)
+
     const movieController = new MovieControllerExpress(movieUseCase)
+
     return new MovieRouterExpress(movieController)
   }
 }
