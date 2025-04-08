6bf92080f9fc9a7e82663c1b17aee127889431c8
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-04-08 17:24:01 -0500

    sync

--- nrc-18307/lab-martes/movies-mvc-frontend/build/js/index/view/IndexView.js
+++ nrc-18307/lab-martes/movies-mvc-frontend/build/js/index/view/IndexView.js
@@ -1,8 +1,12 @@
 export default class IndexView {
+    h1;
     constructor() {
         console.log('IndexView initialized');
+        this.h1 =
+            document.querySelector('header h1') ?? document.createElement('h1');
     }
     render() {
         console.log('Rendering Index View');
+        this.h1.textContent = 'Hello World';
     }
 }
--- nrc-18307/lab-martes/movies-mvc-frontend/src/index/view/IndexView.ts
+++ nrc-18307/lab-martes/movies-mvc-frontend/src/index/view/IndexView.ts
@@ -1,9 +1,14 @@
 export default class IndexView {
+  private readonly h1: HTMLElement
+
   constructor() {
     console.log('IndexView initialized')
+    this.h1 =
+      document.querySelector('header h1') ?? document.createElement('h1')
   }
 
   render() {
     console.log('Rendering Index View')
+    this.h1.textContent = 'Hello World'
   }
 }
