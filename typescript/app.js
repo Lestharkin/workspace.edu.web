813b94a0547834142768a4baaf4f9ef52e8b34d1
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-07-22 17:31:26 -0500

    sync

--- typescript/app.js
+++ typescript/app.js
@@ -13,45 +13,57 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-console.log("TS -> JS");
+console.log('TS -> JS');
 var _num = 123;
-console.log("Number int = ".concat(_num.toString() + " "));
+console.log("Number int = ".concat(_num.toString() + ' '));
 _num = 123.456;
-console.log("Number float = ".concat(_num.toString() + " "));
+console.log("Number float = ".concat(_num.toString() + ' '));
 var _boo = true;
 console.log("Boolean = ".concat(_boo.toString()));
-var _str = "text";
+var _str = 'text';
 console.log("String = ".concat(_str.toString()));
-var _any = "any";
+var _any = 'any';
 console.log("Any = ".concat(_any.toString()));
 _any = false;
 console.log("Any = ".concat(_any.toString()));
 var PI = 3.1416;
 console.log("Constant: PI = ".concat(PI.toString()));
 // num to string
-_str = 123 + " string";
+_str = 123 + ' string';
 console.log("toConcatenate = ".concat(_str));
 console.log("# Arrays");
 var out;
 var _numArray = [0, 1, 2];
 out = 'Number array = [';
-_numArray.forEach(function (i) { out += " ".concat(i, " "); });
+_numArray.forEach(function (i) {
+    out += " ".concat(i, " ");
+});
 console.log(out + ']');
 var _booArray = [true, false];
 out = 'Boolean array = [';
-_booArray.forEach(function (i) { out += " ".concat(i, " "); });
+_booArray.forEach(function (i) {
+    out += " ".concat(i, " ");
+});
 console.log(out + ']');
-var _strArray = ["text0", "text1", "test2"];
+var _strArray = ['text0', 'text1', 'test2'];
 out = 'String array = [';
-_strArray.forEach(function (i) { out += " ".concat(i, " "); });
+_strArray.forEach(function (i) {
+    out += " ".concat(i, " ");
+});
 console.log(out + ']');
-var _arrayAny = [0, true, "text0", [1, 2, 3], { attr1: "value 1", attr2: "value 2" }];
+var _arrayAny = [
+    0,
+    true,
+    'text0',
+    [1, 2, 3],
+    { attr1: 'value 1', attr2: 'value 2' },
+];
 out = 'Any array = [';
 _arrayAny.forEach(function (index) {
-    if (typeof (index) === "number") {
+    if (typeof index === 'number') {
         out += " ".concat(index, " ");
     }
-    if (typeof (index) === "string") {
+    if (typeof index === 'string') {
         out += " ".concat(index, " ");
     }
     if (Array.isArray(index)) {
@@ -61,15 +73,15 @@ _arrayAny.forEach(function (index) {
 out += "{".concat(Object.keys(_arrayAny[4]).map(function (key) { return "".concat(key, ": ").concat(_arrayAny[4][key]); }), "}");
 console.log(out + ']');
 console.log("# Tuple");
-var _tuple = ["text", 1];
+var _tuple = ['text', 1];
 console.log("Tuple = ".concat(_tuple.toString()));
 console.log("# Void, Undefined, Null");
 var _void = undefined;
-console.log("Void = ".concat(typeof (_void)));
+console.log("Void = ".concat(typeof _void));
 var _null = null;
-console.log("Null = ".concat(typeof (_null)));
+console.log("Null = ".concat(typeof _null));
 var _undefined = undefined;
-console.log("Undefined = ".concat(typeof (_undefined)));
+console.log("Undefined = ".concat(typeof _undefined));
 console.log("# Functions");
 function add(n1, n2) {
     return n1 + n2;
@@ -78,10 +90,10 @@ console.log("add = ".concat(add(1, 3)));
 var less = function (n1, n2) { return n1 - n2; };
 console.log("less = ".concat(less(1, 3)));
 var addFunc = function add(n1, n2) {
-    if (typeof (n1) === "string") {
+    if (typeof n1 === 'string') {
         n1 = parseInt(n1);
     }
-    if (typeof (n2) === "string") {
+    if (typeof n2 === 'string') {
         n2 = parseInt(n2);
     }
     return n1 + n2;
@@ -138,5 +150,11 @@ var ChildOfUser = /** @class */ (function (_super) {
     }
     return ChildOfUser;
 }(User));
-var childOfUser = new ChildOfUser({ id: 98, names: 'javier', lastNames: 'gil', age: 18, address: 'calle 2 con 1' });
+var childOfUser = new ChildOfUser({
+    id: 98,
+    names: 'javier',
+    lastNames: 'gil',
+    age: 18,
+    address: 'calle 2 con 1',
+});
 console.log(childOfUser.getString());
