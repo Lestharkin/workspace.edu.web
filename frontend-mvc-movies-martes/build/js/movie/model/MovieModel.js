995b29f4ed86b42cd99572f72929f11b2e9160ed
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-09-25 17:34:43 -0500

    sync

--- frontend-mvc-movies-martes/build/js/movie/model/MovieModel.js
+++ frontend-mvc-movies-martes/build/js/movie/model/MovieModel.js
@@ -1,5 +1,14 @@
 export default class MovieModel {
-    init = () => {
-        console.log('MovieModel init');
+    movies;
+    constructor() {
+        this.movies = [];
+    }
+    getMovies = async () => {
+        const data = await fetch('./movies-2020s.json');
+        this.movies = await data.json();
+        return this.movies;
+    };
+    init = async () => {
+        await this.getMovies();
     };
 }
--- frontend-mvc-movies-martes/src/movie/model/MovieModel.ts
+++ frontend-mvc-movies-martes/src/movie/model/MovieModel.ts
@@ -1,5 +1,19 @@
+import Movie from '../types/Movie'
+
 export default class MovieModel {
-  readonly init = () => {
-    console.log('MovieModel init')
+  private movies: Movie[]
+
+  constructor() {
+    this.movies = []
+  }
+
+  readonly getMovies = async (): Promise<Movie[]> => {
+    const data = await fetch('./movies-2020s.json')
+    this.movies = await data.json()
+    return this.movies
+  }
+
+  readonly init = async () => {
+    await this.getMovies()
   }
 }
