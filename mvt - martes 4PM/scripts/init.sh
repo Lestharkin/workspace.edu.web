49242db9c1059587096674e743c18341757cd496
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2025-10-14 16:37:48 -0500

    sync

---
+++ mvt - martes 4PM/scripts/init.sh
@@ -0,0 +1,4 @@
+npm init -y
+npm i express morgan cors ejs
+npm i typescript ts-node ts-node-dev jest ts-jest supertest @types/express @types/node @types/ejs @types/jest @types/morgan @types/supertest @types/cors -D
+tsc --init 
