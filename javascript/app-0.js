e7ee48f42eba1be9c4b077fb5618702a74cfda34
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2026-01-20 15:47:27 -0500

    sync

--- javascript/app-0.js
+++ javascript/app-0.js
@@ -4,22 +4,22 @@ líneas  */
 
 // comentario 1 línea
 
-console.log('Hola mundo');
-console.log([1,2,3,4,5]);
-console.table([1,2,3,4,5]);
-console.error("un error");
-console.warn("una advertencia");
+console.log('Hola mundo')
+console.log([1, 2, 3, 4, 5])
+console.table([1, 2, 3, 4, 5])
+console.error("un error")
+console.warn("una advertencia")
 // console.clear();
 
-console.time("time");
-console.warn("contando...");
-console.warn("contando...");
-console.warn("contando...");
-console.warn("contando...");
-console.warn("contando...");
-console.warn("contando...");
-console.warn("contando...");
-console.warn("contando...");
-console.warn("contando...");
-console.warn("contando...");
-console.timeEnd("time");
+console.time("time")
+console.warn("contando...")
+console.warn("contando...")
+console.warn("contando...")
+console.warn("contando...")
+console.warn("contando...")
+console.warn("contando...")
+console.warn("contando...")
+console.warn("contando...")
+console.warn("contando...")
+console.warn("contando...")
+console.timeEnd("time")
--- javascript/app-6.js
+++ javascript/app-6.js
@@ -1,22 +1,22 @@
-"use strict";
+"use strict"
 
-console.log('# Spread Operator');
+console.log('# Spread Operator')
 
-const meses = ['Enero', 'Febrero', 'Marzo', 'abril', 'mayo'];
+const meses = ['Enero', 'Febrero', 'Marzo', 'abril', 'mayo', 12, true]
 
-console.table(meses);
-console.table(meses.length);
+console.table(meses)
+console.table(meses.length)
 
 for (let index = 0; index < meses.length; index++) {
-    console.log(index);
-    console.log(meses[index]);
+    console.log(index)
+    console.log(meses[index])
 }
 
 // Spread Operator
 
-console.log(...meses);
+console.log(...meses)
 
-const bag  = [];
+const bag = []
 
 const producto1 = {
     name: "Moinitor Asus",
@@ -33,45 +33,45 @@ const producto3 = {
     precio: 50,
 }
 
-let resultado = [...bag, producto1];
+let resultado = [...bag, producto1]
 
-resultado = [...resultado, producto2];
-resultado = [producto3, ...resultado];
+resultado = [...resultado, producto2]
+resultado = [producto3, ...resultado]
 
-console.table(resultado);
+console.table(resultado)
 
 const shopcar = [
-    { nombre: 'Monitor HP', precio: 700},
-    { nombre: 'Televisión LG', precio: 1200},
-    { nombre: 'Tablet Samsung', precio: 2000},
-    { nombre: 'Audifonos Buen sonido', precio: 300},
-    { nombre: 'teclado gammer', precio: 400},
-    { nombre: 'Celular Iphone', precio: 18000},
-];
+    { nombre: 'Monitor HP', precio: 700 },
+    { nombre: 'Televisión LG', precio: 1200 },
+    { nombre: 'Tablet Samsung', precio: 2000 },
+    { nombre: 'Audifonos Buen sonido', precio: 300 },
+    { nombre: 'teclado gammer', precio: 400 },
+    { nombre: 'Celular Iphone', precio: 18000 },
+]
 
 for (let index = 0; index < shopcar.length; index++) {
-    console.log(shopcar[index]);
+    console.log(shopcar[index])
 }
 
 for (let index of shopcar) {
-    console.log(shopcar[index]);
+    console.log(shopcar[index])
 }
 
 for (let itemcar in shopcar) {
-    console.log(itemcar);
+    console.log(itemcar)
 }
 
 const nfor = shopcar.forEach((producto) => {
-    return (`${producto.nombre} - Precio: ${producto.precio}`);
-});
+    return (`${producto.nombre} - Precio: ${producto.precio}`)
+})
 
 const nmap = shopcar.map((producto) => {
-    return (`${producto.nombre} - Precio: ${producto.precio}`);
-});
+    return (`${producto.nombre} - Precio: ${producto.precio}`)
+})
 
 
-console.log(nmap);
-console.log(nfor);
+console.log(nmap)
+console.log(nfor)
 
 
 const item = {
@@ -92,10 +92,10 @@ const item = {
     }
 }
 
-console.log(item);
+console.log(item)
 
-const { nombre, informacion, informacion: { fabricacion: { pais } } } = item;
+const { nombre, informacion, informacion: { fabricacion: { pais } } } = item
 
-console.log(nombre);
-console.log(informacion);
-console.log(pais);
+console.log(nombre)
+console.log(informacion)
+console.log(pais)
