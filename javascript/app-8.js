096a0178ecb2cbed5213b4fbe3a7c01590d3d005
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2026-01-26 16:48:42 -0500

    sync

--- javascript/app-8.js
+++ javascript/app-8.js
@@ -1,20 +1,20 @@
-import readline from "readline/promises";
+import readline from "readline/promises"
 
 const prompt = async (message) => {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-  });
+  })
 
-  const answer = await rl.question(message);
+  const answer = await rl.question(message)
 
-  rl.close();
-  return answer;
-};
-
-async function simpleSum() {
-  const a = await prompt("Enter first number: ");
-  const b = await prompt("Enter second number: ");
-  console.log("Sum is", Number(a) + Number(b));
+  rl.close()
+  return answer
 }
-simpleSum();
+
+async function simpleSum () {
+  const a = await prompt("Enter first number: ")
+  const b = await prompt("Enter second number: ")
+  console.log("Sum is", Number(a) + Number(b))
+}
+simpleSum()
