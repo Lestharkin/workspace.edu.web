5858ad3532c9cea252bb9427b8ed7930e864a806
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2026-02-09 16:25:36 -0500

    sync

--- 11712-14/mvc-movies/src/movies/models/MovieModel.ts
+++ 11712-14/mvc-movies/src/movies/models/MovieModel.ts
@@ -1,3 +1,12 @@
 import movies_json from '../../../database/movies-2020s.json'
+import Movie from '../types/Movie'
 
-export default class MovieModel {}
+export default class MovieModel {
+  readonly fetchMovieList = async (): Promise<Movie[]> => {
+    const movies = movies_json as Movie[]
+    if (!movies) {
+      return []
+    }
+    return movies
+  }
+}
