8f0713739c2a6e868a9dc80c76cd0eff37ca622f
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2026-02-09 16:54:22 -0500

    sync

--- 11712-14/mvc-movies/src/movies/controllers/MovieController.ts
+++ 11712-14/mvc-movies/src/movies/controllers/MovieController.ts
@@ -4,8 +4,8 @@ import MovieModel from '../models/MovieModel'
 export default class MovieController {
   constructor(private readonly movieModel: MovieModel) {}
 
-  readonly getMovieList = (_req: Request, res: Response) => {
-    const movies = this.movieModel.fetchMovieList()
+  readonly getMovieList = async (_req: Request, res: Response) => {
+    const movies = await this.movieModel.fetchMovieList()
 
     res.status(200).json(movies)
   }
