2e1eea1530823fb6b94e0bfec82a9319b0a3fb76
Lenin Javier Serrano Gil <sg.leninj@gmail.com>
2026-04-07 15:45:12 -0500

    sync

--- 11712-14/frontend-mvc/js/index.js
+++ 11712-14/frontend-mvc/js/index.js
@@ -1,9 +1,35 @@
-const btnReset = document.createElement("button")
+const user = {
+  username: "yo",
+  passwd: "yo2"
+}
 
-btnReset.setAttribute("type", "reset")
-btnReset.setAttribute("class", "btn btn-secondary")
-btnReset.textContent = "Reset"
+function load () {
+  const btnReset = document.createElement("button")
 
-const btns = document.getElementById("btns")
+  btnReset.setAttribute("type", "reset")
+  btnReset.setAttribute("class", "btn btn-secondary")
+  btnReset.textContent = "Reset"
 
-btns.appendChild(btnReset)
+  const btns = document.getElementById("btns")
+
+  btns.appendChild(btnReset)
+
+  const submit = document.getElementById("submit")
+  submit.addEventListener("click", (event) => {
+    event.preventDefault()
+
+    const email = document.getElementById("email")
+    const password = document.getElementById("password")
+    const x = document.querySelector("#emailHelp")
+
+    if (email.value === user.username && password.value === user.passwd) {
+      alert("Login successful")
+    } else {
+      x.classList.add("colorRojo")
+    }
+  })
+}
+
+window.onload = load
+
+
